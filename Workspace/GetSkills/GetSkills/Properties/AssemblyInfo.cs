a1a775b5aec049142768af74b4cad8ee0a887aa4
YsqEvilmax <ysq.evilmax@qq.com>
2015-08-15 17:33:34 +1200

    1.Fix version confilict 2.Add resources folders

--- Workspace/GetSkills/GetSkills/Properties/AssemblyInfo.cs
+++ Workspace/GetSkills/GetSkills/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
-[assembly: Guid("60107bdf-a495-4808-b943-2a53818df1f7")]
+[assembly: Guid("36a343b1-1ce3-4413-a8ca-ae76040050ec")]
 
 // 程序集的版本信息由下列四个值组成:
 //
