f34f10048d3eeec561f15385f349a89f5e534ab3
Kaii <kaikakin@hotmail.com>
2015-09-07 22:10:34 +1200

    Update success story list for Admin; Add Test project; Add Font Awesome package.  (Kaii)

---
+++ Workspace/GetSkills/GetSkillsTests/Controllers/SuccessStoryControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GetSkills.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSkills.Models.Tests
+{
+    [TestClass()]
+    public class SuccessStoryControllerTests
+    {
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
