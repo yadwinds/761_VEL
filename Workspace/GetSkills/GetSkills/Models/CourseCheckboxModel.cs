8efd9c0e9ed2389c72d41142a5492cb95aaa4200
Kaii <kaikakin@hotmail.com>
2015-09-09 18:55:01 +1200

    Update for the change of model class. (Kaii)

--- Workspace/GetSkills/GetSkills/Models/CourseCheckboxModel.cs
+++ Workspace/GetSkills/GetSkills/Models/CourseCheckboxModel.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace GetSkills.Models
 {
-    public class CourseCheckboxModel : cours
+    public class CourseCheckboxModel : course
     {
         public int story_course_id { get; set; }
         public bool isSelected { get; set; }
