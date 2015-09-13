b2c08997bc1e9bd9706026ebc034b8a06b35fd66
lulujing <893905340@qq.com>
2015-09-13 15:50:57 +0800

    testcase

--- Workspace/GetSkills/GetSkills.Tests/Controllers/SuccessStoryControllerTests.cs
+++ Workspace/GetSkills/GetSkills.Tests/Controllers/SuccessStoryControllerTests.cs
@@ -17,9 +17,10 @@ namespace GetSkills.Models.Tests
         public async Task SuccessStoryController_Index_Test01()
         {
             SuccessStoryController ctl = new SuccessStoryController();
-            var result = await ctl.Index("ID_asc");
 
+            var result = await ctl.Index("ID_asc");
             Assert.IsNotNull(result);
+
             var modelList = (List<StoryIndexViewModel>)((ViewResult)result).Model;
             Assert.AreEqual(6, modelList.Count());
         }
@@ -28,9 +29,10 @@ namespace GetSkills.Models.Tests
         public async Task SuccessStoryController_Index_Test02()
         {
             SuccessStoryController ctl = new SuccessStoryController();
-            var result = await ctl.Index("ID_desc");
 
+            var result = await ctl.Index("ID_desc");
             Assert.IsNotNull(result);
+
             var modelList = (List<StoryIndexViewModel>)((ViewResult)result).Model;
             Assert.AreEqual(6, modelList.Count());
         }
@@ -39,12 +41,170 @@ namespace GetSkills.Models.Tests
         public async Task SuccessStoryController_Index_Test03()
         {
             SuccessStoryController ctl = new SuccessStoryController();
-            var result = await ctl.Index("Order_desc");
 
+            var result = await ctl.Index("Order_desc");
             Assert.IsNotNull(result);
+
             var modelList = (List<StoryIndexViewModel>)((ViewResult)result).Model;
             Assert.AreEqual(6, modelList.Count());
         }
 
+        [TestMethod()]
+        public async Task SuccessStoryController_Index_Test04()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+
+            var result = await ctl.Index("Order_asc");
+            Assert.IsNotNull(result);
+
+            var modelList = (List<StoryIndexViewModel>)((ViewResult)result).Model;
+            Assert.AreEqual(6, modelList.Count());
+        }
+
+        [TestMethod()]
+        public async Task SuccessStoryController_List_Test()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+
+            var result = await ctl.List();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public async Task SuccessStoryController_Detail_Test01()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+
+            var result = await ctl.Detail(1);
+            Assert.IsNotNull(result);
+
+            var model = (StoryIndexViewModel)((ViewResult)result).Model;
+            Assert.IsNotNull(model);
+
+        }
+
+        [TestMethod()]
+        public async Task SuccessStoryController_Detail_Test02()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+
+            var result = await ctl.Detail(21);
+            Assert.IsNotNull(result);
+
+            var model = (StoryIndexViewModel)((ViewResult)result).Model;
+            Assert.IsNotNull(model);
+        }
+
+        [TestMethod()]
+        public async Task SuccessStoryController_Detail_Test03()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+
+            var result = await ctl.Detail(31);
+            Assert.IsNotNull(result);
+
+            var model = (StoryIndexViewModel)((ViewResult)result).Model;
+            Assert.IsNotNull(model);
+        }
+
+        [TestMethod()]
+        public async Task SuccessStoryController_Detail_Test04()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+
+            var result = await ctl.Detail(41);
+            Assert.IsNotNull(result);
+
+            var model = (StoryIndexViewModel)((ViewResult)result).Model;
+            Assert.IsNotNull(model);
+        }
+
+        [TestMethod()]
+        public async Task SuccessStoryController_Detail_Test05()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+
+            var result = await ctl.Detail(51);
+            Assert.IsNotNull(result);
+
+            var model = (StoryIndexViewModel)((ViewResult)result).Model;
+            Assert.IsNotNull(model);
+        }
+
+        [TestMethod()]
+        public async Task SuccessStoryController_Detail_Test06()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+
+            var result = await ctl.Detail(61);
+            Assert.IsNotNull(result);
+
+            var model = (StoryIndexViewModel)((ViewResult)result).Model;
+            Assert.IsNotNull(model);
+        }
+
+        [TestMethod()]
+        public async Task SuccessStoryController_Detail_Test07()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+
+            var result = await ctl.Detail(71);
+            Assert.IsNotNull(result);
+
+            var model = (StoryIndexViewModel)((ViewResult)result).Model;
+            Assert.IsNotNull(model);
+        }
+
+        [TestMethod()]
+        public async Task SuccessStoryController_Detail_Test08()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+
+            var result = await ctl.Detail(0);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(HttpNotFoundResult));
+        }
+
+        [TestMethod()]
+        public async Task SuccessStoryController_Detail_Test09()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+
+            var result = await ctl.Detail(-11);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(HttpNotFoundResult));
+        }
+
+        [TestMethod()]
+        public async Task SuccessStoryController_Detail_Test10()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+
+            var result = await ctl.Detail(101);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(HttpNotFoundResult));
+        }
+
+        [TestMethod()]
+        public void SuccessStoryController_Create_Test01()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+
+            var result = ctl.Create();
+            Assert.IsNotNull(result);
+
+            var model = (StoryIndexViewModel)((ViewResult)result).Model;
+            Assert.AreEqual("~/Images/no_image.jpg", model.successStory.pic);
+        }
+
+        [TestMethod()]
+        public void SuccessStoryController_Create_Test02()
+        {
+            SuccessStoryController ctl = new SuccessStoryController();
+            StoryIndexViewModel editView = new StoryIndexViewModel();
+
+            var result = ctl.Create(editView);
+            Assert.IsNotNull(result);
+        }
     }
 }
