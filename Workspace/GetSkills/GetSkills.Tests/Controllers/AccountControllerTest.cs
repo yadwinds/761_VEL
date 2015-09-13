7a6fc74d6857e75ffd9e4d0ff305a1ba4c1df5f3
lulujing <893905340@qq.com>
2015-09-13 16:00:04 +0800

    test case

--- Workspace/GetSkills/GetSkills.Tests/Controllers/AccountControllerTest.cs
+++ Workspace/GetSkills/GetSkills.Tests/Controllers/AccountControllerTest.cs
@@ -1,14 +1,35 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GetSkills.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GetSkills.Controllers;
+using System.Web.Mvc;
 namespace GetSkills.Tests.Controllers
 {
     [TestClass]
     public class AccountControllerTest
     {
+        private string returnUrl;
+
+        public object ViewBag { get; private set; }
+
         [TestMethod]
-        public void TestMethod1()
+        public void Login()
         {
+            // Arrange
+            AccountController controller = new AccountController();
+            String f=returnUrl;
+            // Act
+
+            ViewResult result = controller.Login(returnUrl) as ViewResult; 
+
+            // Assert
+            Assert.IsNotNull(result);
         }
+
+
     }
-}
+    }
