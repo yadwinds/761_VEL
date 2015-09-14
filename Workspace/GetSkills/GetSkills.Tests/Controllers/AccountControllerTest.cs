298801e046741a6f6a0873d7a358e166c5400578
lulujing <893905340@qq.com>
2015-09-14 18:11:10 +0800

    test case

--- Workspace/GetSkills/GetSkills.Tests/Controllers/AccountControllerTest.cs
+++ Workspace/GetSkills/GetSkills.Tests/Controllers/AccountControllerTest.cs
@@ -13,12 +13,36 @@ namespace GetSkills.Tests.Controllers
     public class AccountControllerTest
     {
         [TestMethod]
-        public void AccountController_Login_Test01()
+        public void AccountController_AdminLogin_Test01()
         {
-<<<<<<< HEAD
             // Initialization
             AccountController ctl = new AccountController();
-            String url = "http://getskills.azurewebsites.net/";
+            string url = "http://getskills.azurewebsites.net/";
+
+            // To test if it is null
+            ViewResult result = ctl.AdminLogin(url) as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void AccountController_AdminLogin_Test02()
+        {
+            // Initialization
+            AccountController ctl = new AccountController();
+            user model = new user();
+            string url = "http://getskills.azurewebsites.net/";
+
+            // To test if it is null
+            var result = ctl.AdminLogin(model, url);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void AccountController_Login_Test01()
+        {
+            // Initialization
+            AccountController ctl = new AccountController();
+            string url = "http://getskills.azurewebsites.net/";
 
             // To test if it is null
             ViewResult result = ctl.Login(url) as ViewResult; 
@@ -34,7 +58,7 @@ namespace GetSkills.Tests.Controllers
             model.Email = "aaaa";
             model.Password = "123456";
             model.RememberMe = false;
-            String url = "http://getskills.azurewebsites.net/";
+            string url = "http://getskills.azurewebsites.net/";
 
             // To test if it is null
             var result = ctl.Login(model, url);
@@ -42,43 +66,27 @@ namespace GetSkills.Tests.Controllers
         }
 
         [TestMethod]
-        public void AccountController_VerifyCode_Test()
+        public void AccountController_VerifyCode_Test01()
         {
             // Initialization
             AccountController ctl = new AccountController();
-            VerifyCodeViewModel model = null;
+            string provider = "Admin";
+            string url = "http://getskills.azurewebsites.net/";
+            bool rememberMe = false;
 
             // To test if it is null
-=======
-            AccountController ctl = new AccountController();
-            String url = "http://getskills.azurewebsites.net/";
-          
-            ViewResult result = ctl.Login(url) as ViewResult; 
-            Assert.IsNotNull(result);
+            var result = ctl.VerifyCode(provider, url, rememberMe);
+            Assert.IsNotNull(result);          
         }
 
-         [TestMethod]
-         public void AccountController_Login_Test02()
-         {
-             AccountController ctl = new AccountController();
-             LoginViewModel model = new LoginViewModel();
-             model.Email = "aaaa";
-             model.Password = "123456";
-             model.RememberMe = false;
-
-             String url = "http://getskills.azurewebsites.net/";
-
-             var result = ctl.Login(model,url);
-             Assert.IsNotNull(result);
-         }
-
         [TestMethod]
-        public void AccountController_VerifyCode_Test()
+        public void AccountController_VerifyCode_Test02()
         {
+            // Initialization
             AccountController ctl = new AccountController();
-            VerifyCodeViewModel model = null;
+            VerifyCodeViewModel model = new VerifyCodeViewModel();
 
->>>>>>> origin/master
+            // To test if it is null
             var result = ctl.VerifyCode(model);
             Assert.IsNotNull(result);
         }
@@ -86,7 +94,6 @@ namespace GetSkills.Tests.Controllers
         [TestMethod]
         public void AccountController_Regitser_Test01()
         {
-<<<<<<< HEAD
             // Initialization
             AccountController ctl = new AccountController();
 
@@ -100,7 +107,7 @@ namespace GetSkills.Tests.Controllers
         {
             // Initialization
             AccountController ctl = new AccountController();
-            RegisterViewModel model = null;
+            RegisterViewModel model = new RegisterViewModel();
 
             // To test if it is null
             var result = ctl.Register(model);
@@ -112,26 +119,64 @@ namespace GetSkills.Tests.Controllers
         {
             // Initialization
             AccountController ctl = new AccountController();
-            RegisterViewModel model = null;
+            string id = "0";
+            string code = "888888";
 
             // To test if it is null
-            var result = ctl.Register(model);
+            var result = ctl.ConfirmEmail(id, code);
             Assert.IsNotNull(result);
-        }
-=======
-            AccountController ctl = new AccountController();
->>>>>>> origin/master
 
-            var result = ctl.Register();
+            // Check the content
+            Assert.AreEqual("ConfirmEmail", result.Succeeded);
+        }
+
+        [TestMethod]
+        public void AccountController_ForgotPassword_Test01()
+        {
+            // Initialization
+            AccountController ctl = new AccountController();
+
+            // To test if it is null
+            var result = ctl.ForgotPassword();
             Assert.IsNotNull(result);
         }
 
         [TestMethod]
-        public void AccountController_Regitser_Test02()
+        public void AccountController_ForgotPassword_Test02()
         {
+            // Initialization
             AccountController ctl = new AccountController();
-            RegisterViewModel model = null;
-            var result = ctl.Register(model);
+            ForgotPasswordViewModel model = new ForgotPasswordViewModel();
+
+            // To test if it is null
+            var result = ctl.ForgotPassword(model);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void AccountController_ResetPassword_Test01()
+        {
+            // Initialization
+            AccountController ctl = new AccountController();
+            string code = "888888";
+
+            // To test if it is null
+            var result = ctl.ResetPassword(code);
+            Assert.IsNotNull(result);
+
+            // Check the content
+            Assert.AreEqual("Error", result);
+        }
+
+        [TestMethod]
+        public void AccountController_ResetPassword_Test02()
+        {
+            // Initialization
+            AccountController ctl = new AccountController();
+            ResetPasswordViewModel model = new ResetPasswordViewModel();
+
+            // To test if it is null
+            var result = ctl.ResetPassword(model);
             Assert.IsNotNull(result);
         }
     }
