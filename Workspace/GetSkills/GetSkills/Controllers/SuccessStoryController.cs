5b7fba3121e4bef756efcb8f97fe8813ac4706cb
yinqi522 <1251226696@qq.com>
2015-10-05 18:24:39 +1300

    update readme

--- Workspace/GetSkills/GetSkills/Controllers/SuccessStoryController.cs
+++ Workspace/GetSkills/GetSkills/Controllers/SuccessStoryController.cs
@@ -192,8 +192,6 @@ namespace GetSkills.Controllers
         }
 
         // POST: SuccessStory/Create
-        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
-        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create(StoryIndexViewModel editView)
