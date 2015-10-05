baa92750c921b284994b4f90088737a7d057ff30
gaoyiqun <ygao742@aucklanduni.ac.nz>
2015-10-06 03:13:34 +1300

    Changed the way to implement WebsiteVisitorCounter. Use txt file to record the visitor number imformation.

--- Workspace/GetSkills/GetSkills/Controllers/HomeController.cs
+++ Workspace/GetSkills/GetSkills/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+
 
 namespace GetSkills.Controllers
 {
@@ -10,7 +12,31 @@ namespace GetSkills.Controllers
     {
         public ActionResult Index()
         {
+            // Add Session ID and Guid, Set up allowed response time
+            System.Web.HttpContext.Current.Session.Add("SessionID", Guid.NewGuid().ToString());
+            System.Web.HttpContext.Current.Session.Timeout = 5;
+
+            string[] data = null;
+            string path = Server.MapPath("~/App_Data/count.txt");       
+            System.Web.HttpContext.Current.Application.Lock();      //Block the application to get the sync data
+            if (System.IO.File.Exists(path))        //If count.txt exists, read the data.
+            {
+                data = System.IO.File.ReadAllLines(path);
+            }
+            else                                    //If count.txt doesn't exists, create the file and set up to start count from 0.
+            {
+                System.IO.File.Create(Server.MapPath("~/App_Data/count.txt")).Dispose();
+                data = new string[1] { "0" };
+            }
+            
+            System.Web.HttpContext.Current.Application["count"] = Convert.ToInt32(data[0]) + 1; //Get currently online visitors amount and then add 1
+            string count = System.Web.HttpContext.Current.Application["count"].ToString();
+        
+            System.IO.File.WriteAllText(path, count);           //Write data into file
+            System.Web.HttpContext.Current.Application.UnLock();       //Unlock Application session
+
             return View();
+  
         }
 
         public ActionResult About()
--- Workspace/GetSkills/GetSkills/Global.asax.cs
+++ Workspace/GetSkills/GetSkills/Global.asax.cs
@@ -18,6 +18,17 @@ namespace GetSkills
             //This configration squash scripts to clients, minimizing file size and preventing hacking.
             BundleTable.EnableOptimizations = true;
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            Application.Lock();      //临界变量,使用加锁功能,其他用户不能访问。
+            Application["count"] = 0;
+            Application.UnLock();     //临界变量被解锁。
+
+        }
+
+        protected void Application_End()
+        {
+
         }
     }
 }
